7677323ad3b1f04baa4f22105a5fddddb143b608
gnemek20 <gnemek20@gmail.com>
2023-03-23 20:43:13 +0900

    vue-session 추가

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import store from './store'
 import functions from '@/scripts/functions.js'
 Vue.use(functions)
 
+import VueSession from 'vue-session'
+const sessionOptions = { persist: true }
+Vue.use(VueSession, sessionOptions)
+
 Vue.config.productionTip = false
 
 new Vue({
