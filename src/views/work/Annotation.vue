ed62871522a8221cae736f3d560013db84950f27
gnemek20 <gnemek20@gmail.com>
2023-03-26 22:37:37 +0900

    canvas에 그림 그리는 기능 추가

--- src/views/work/Annotation.vue
+++ src/views/work/Annotation.vue
@@ -4,24 +4,90 @@
       <h1>Annotation</h1>
       <hr>
     </div>
-    <div class="canvas">
-      <canvas ref="image" width="800" height="800"></canvas>
+    <div class="board">
+      <canvas ref="imageCanvas" width="798" height="798"></canvas>
+      <canvas :ref="`boxCanvas${index}`" v-for="(box, index) in boxes" v-bind:key="index" width="798" height="798"></canvas>
+      <canvas ref="dragCanvas" width="798" height="798"></canvas>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      canvas: {
+        imageCanvas: Object,
+        dragCanvas: Object
+      },
+      context: {
+        imageContext: Object,
+        dragContext: Object
+      },
+      boxes: [],
+      boxesLength: Number
+    }
+  },
   mounted() {
-    const canvas = this.$refs['image'];
-    const context = canvas.getContext('2d');
+    this.canvas.imageCanvas = this.$refs['imageCanvas'];
+    this.context.imageContext = this.canvas.imageCanvas.getContext('2d');
 
     let getImage = new Image();
     getImage.src = `https://drive.google.com/uc?export=view&id=${this.$session.get('image')}`;
     getImage.onload = () => {
-      context.drawImage(getImage, 0, 0, canvas.width, canvas.height);
-      console.log(getImage.width)
-      console.log(getImage.height)
+      this.context.imageContext.drawImage(getImage, 0, 0, this.canvas.imageCanvas.width, this.canvas.imageCanvas.height);
+    }
+
+    this.canvas.dragCanvas = this.$refs['dragCanvas'];
+    this.context.dragContext = this.canvas.dragCanvas.getContext('2d');
+
+    this.canvas.dragCanvas.addEventListener('mousedown', this.mouseDown);
+    this.canvas.dragCanvas.addEventListener('mouseup', this.mouseUp);
+  },
+  methods: {
+    dragBox(context, x, y, dx, dy) {
+      context.setLineDash([10]);
+      context.clearRect(0, 0, this.canvas.dragCanvas.width, this.canvas.dragCanvas.height);
+      this.drawBox(context, x, y, dx, dy);
+    },
+    drawBox(context, x, y, dx, dy) {
+      context.beginPath();
+      context.moveTo(x, y);
+      context.lineTo(dx, y);
+      context.lineTo(dx, dy);
+      context.lineTo(x, dy);
+      context.closePath();
+      context.stroke();
+    },
+
+    /* Drag Event */
+    mouseDown(event) {
+      this.boxesLength = this.boxes.length;
+      this.boxes.push({
+        x: event.offsetX,
+        y: event.offsetY,
+        dx: Number,
+        dy: Number
+      });
+
+      this.canvas.dragCanvas.addEventListener('mousemove', this.mouseMove);
+    },
+    mouseMove(event) {
+      this.dragBox(this.context.dragContext, this.boxes[this.boxesLength].x, this.boxes[this.boxesLength].y, event.offsetX, event.offsetY);
+    },
+    mouseUp(event) {
+      this.canvas.dragCanvas.removeEventListener('mousemove', this.mouseMove);
+      this.context.dragContext.clearRect(0, 0, this.canvas.dragCanvas.width, this.canvas.dragCanvas.height);
+
+      this.boxes[this.boxesLength].dx = Math.max(this.boxes[this.boxesLength].x, event.offsetX);
+      this.boxes[this.boxesLength].dy = Math.max(this.boxes[this.boxesLength].y, event.offsetY);
+      this.boxes[this.boxesLength].x = Math.min(this.boxes[this.boxesLength].x, event.offsetX);
+      this.boxes[this.boxesLength].y = Math.min(this.boxes[this.boxesLength].y, event.offsetY);
+
+      const canvas = this.$refs[`boxCanvas${this.boxesLength}`][0];
+      const context = canvas.getContext('2d');
+
+      this.drawBox(context, this.boxes[this.boxesLength].x, this.boxes[this.boxesLength].y, this.boxes[this.boxesLength].dx, this.boxes[this.boxesLength].dy);
     }
   }
 }
@@ -37,11 +103,12 @@ export default {
   }
 }
 
-.canvas {
+.board {
   display: flex;
   justify-content: center;
 
   canvas {
+    position: absolute;
     border: 1px solid dimgray;
   }
 }
