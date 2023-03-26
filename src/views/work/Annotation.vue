25caa16d0863e904d30dfdbb69d023e36a128d17
gnemek20 <gnemek20@gmail.com>
2023-03-26 19:05:32 +0900

    annotation 페이지에서 이미지 로드 기능 추가

--- src/views/work/Annotation.vue
+++ src/views/work/Annotation.vue
@@ -4,8 +4,28 @@
       <h1>Annotation</h1>
       <hr>
     </div>
+    <div class="canvas">
+      <canvas ref="image" width="800" height="800"></canvas>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const canvas = this.$refs['image'];
+    const context = canvas.getContext('2d');
+
+    let getImage = new Image();
+    getImage.src = `https://drive.google.com/uc?export=view&id=${this.$session.get('image')}`;
+    getImage.onload = () => {
+      context.drawImage(getImage, 0, 0, canvas.width, canvas.height);
+      console.log(getImage.width)
+      console.log(getImage.height)
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .title {
@@ -14,6 +34,15 @@
 
   h1 {
     margin-bottom: 10px;
+  }
+}
+
+.canvas {
+  display: flex;
+  justify-content: center;
+
+  canvas {
+    border: 1px solid dimgray;
   }
 }
 </style>
