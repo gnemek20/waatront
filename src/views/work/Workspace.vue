054b5307c71c8839b071678c50d8748bc212efd9
gnemek20 <gnemek20@gmail.com>
2023-03-31 13:00:10 +0900

    세션 제거 기능 추가, 코코 데이터셋과 어노테이션 등의 정보를 저장하는 기능 추가, 어노테이션 등의 정보를 가져와서 세션에 등록하는 기능 추가

--- src/views/work/Workspace.vue
+++ src/views/work/Workspace.vue
@@ -4,6 +4,9 @@
       <h1>Workspace</h1>
       <hr>
     </div>
+    <div class="option">
+      <button @click="getCoco">coco</button>
+    </div>
     <div class="list">
       <div class="image" @click="$push(`/annotation?image=${image.name}`)" v-for="(image, index) in images" v-bind:key="index">
         <canvas :ref="image.name" width="148" height="148" />
@@ -26,15 +29,46 @@ export default {
     }
   },
   async beforeCreate() {
-    const { status, data } = await this.$post('/drive/images', {
+    const save = await this.$post('/drive/getSave', {
       workspace: this.$session.get('workspace')
     });
 
-    if (status === 200) {
-      data.forEach((data) => {
+    const saveString = save.data;
+    const categoriesString = saveString.split('\n')[0].split('|');
+    const annotationsString = saveString.split('\n')[1].split('|');
+
+    const categories = [];
+    for (let i = 1; i < categoriesString.length; i++) {
+      categories.push({
+        name: categoriesString[i]
+      });
+    }
+    this.$session.set('categories', categories);
+
+    const annotations = [];
+    for (let i = 1; i < annotationsString.length; i++) {
+      const annotationString = annotationsString[i].split('@');
+      annotations.push({
+        image: annotationString[0],
+        canvasIndex: annotationString[1],
+        name: annotationString[2],
+        x: annotationString[3],
+        y: annotationString[4],
+        dx: annotationString[5],
+        dy: annotationString[6]
+      });
+    }
+    this.$session.set('annotations', annotations);
+
+    const images = await this.$post('/drive/images', {
+      workspace: this.$session.get('workspace')
+    });
+
+    if (images.status === 200) {
+      images.data.forEach((image) => {
         this.images.push({
-          id: data.id,
-          name: data.name,
+          id: image.id,
+          name: image.name,
           width: 0,
           height: 0
         });
@@ -55,6 +89,19 @@ export default {
         this.$session.set('images', this.images);
       }
     }
+  },
+  beforeRouteLeave(to, from, next) {
+    this.updateCoco();
+    this.updateSave();
+
+    if (to.name !== 'annotation') {
+      this.$session.remove('workspace');
+      this.$session.remove('images');
+      this.$session.remove('categories');
+      this.$session.remove('annotations');
+    }
+ 
+    next();
   },
   methods: {
     async uploadImage() {
@@ -84,20 +131,36 @@ export default {
         }
       }
     },
-    async uploadCoco() {
+    async getCoco() {
+      const { status, data } = await this.$post('/drive/getCoco', {
+        workspace: this.$session.get('workspace')
+      });
+
+      if (status === 200) {
+        window.open(`https://drive.google.com/file/d/${data}`);
+      }
+    },
+    async updateCoco() {
       const images = this.$session.get('images');
       const categories = this.$session.get('categories');
       const annotations = this.$session.get('annotations');
 
-      const { status, data } = await this.$post('/drive/uploadCoco', {
+      const { status, data } = await this.$post('/drive/updateCoco', {
+        workspace: this.$session.get('workspace'),
         images: images,
         categories: categories,
         annotations: annotations
       });
+    },
+    async updateSave() {
+      const categories = this.$session.get('categories');
+      const annotations = this.$session.get('annotations');
 
-      if (status === 200) {
-        console.log(data)
-      }
+      const { status, data } = await this.$post('/drive/updateSave', {
+        workspace: this.$session.get('workspace'),
+        categories: categories,
+        annotations: annotations
+      });
     }
   }
 }
@@ -110,6 +173,12 @@ export default {
   h1 {
     margin-bottom: 10px;
   }
+}
+.option {
+  display: flex;
+  width: 800px;
+  margin: 0px auto 20px;
+  justify-content: end;
 }
 .list {
   display: grid;
